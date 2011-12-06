288679b89809813b126a393e013255538df34bd2
Joao Campos <samura@gmail.com>
2011-12-06 15:56:02 +0000

    #3 - i18n translations

---
+++ lib/form/doctrine/PluginProductCategoryTranslationForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PluginProductCategoryTranslation form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage form
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfDoctrineFormPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+abstract class PluginProductCategoryTranslationForm extends BaseProductCategoryTranslationForm
+{
+}
