8cdfd5f61e3774d8419bf6d4a700e8b82eff161e
Flávio Ferreira <flavioxavier@gmail.com>
2011-12-06 18:34:50 +0000

    list products

--- modules/aProductItem/templates/indexSuccess.php
+++ modules/aProductItem/templates/indexSuccess.php
@@ -1,8 +1,39 @@
-<?php use_helper('a') ?>
+<?php use_helper('a','JavascriptBase', 'I18N') ?>
+<?php $admin = $sf_user->isAuthenticated(); ?>
 
 <?php // Defining the <body> class ?>
 <?php slot('a-body-class','a-product index') ?>
 
-<div class="product-list">
-<?php include_component('aProductItem','navigation') ?>
+<div class="category-list">
+
+<?php if($admin):  // adds new category ?>
+    <?php echo a_js_button('<span class="icon"></span>Add Category', array('a-btn','icon', 'a-add', 'add-product-category')) ?>
+    <div class="add">
+      <?php echo form_tag('aProductItem_category_add') ?>
+      <input type="text" name="category">
+      <?php echo a_js_button('Save', array('a-btn','a-save', 'save-product-category')) ?>
+      </form>
+    </div>
+<?php endif ?>
+
+<ul class="categories a-ui">
+<?php foreach($categories as $category): ?>
+  <li class="<?php echo $category->slug ?>"><?php include_partial('category_title', array('category' => $category)) ?></li>
+<?php endforeach; ?>
+</ul>
 </div>
+
+<?php //include_component('aProductItem','navigation') ?>
+
+
+
+<?php javascript_tag() ?>
+$('.add-product-category, .add-product').click(function(){
+  $(this).hide();
+  $(this).next().show();
+});
+
+$('.save-product-category, .save-product').click(function(){
+  $(this).parent().submit();
+});
+<?php end_javascript_tag() ?>
