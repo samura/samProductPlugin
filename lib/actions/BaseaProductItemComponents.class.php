24e7b30525b59873cdb1d94b778d8978504c207a
Joao Campos <samura@gmail.com>
2011-12-08 21:37:42 +0000

    Add components base class

--- lib/actions/BaseaProductItemComponents.class.php
+++ lib/actions/BaseaProductItemComponents.class.php
@@ -8,6 +8,10 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class aProductItemComponents extends BaseaProductItemComponents
+class BaseaProductItemComponents extends sfComponents
 {
+  public function executeSubnav(sfWebRequest $request)
+  {
+    $this->product_categories = Doctrine::getTable('ProductCategory')->createQuery()->execute();
+  }
 }
